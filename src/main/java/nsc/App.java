97df5966709bb5ef597a0359558aecb75a3d695a
chaluemwuut <chaluemwut@hotmail.com>
2019-11-02 19:07:20 +0700

    first draft

--- src/main/java/nsc/App.java
+++ src/main/java/nsc/App.java
@@ -1,15 +1,18 @@
 package nsc;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App {
 
     public static void main(String[] args) {
-        List lst = Stream.of(1, 2, 3, 4, 45, 5, 6)
-                .filter(e -> e > 5)
-                .collect(Collectors.toList());
-        System.out.println(lst);
+        List lst = List.of("a", "b", "c", "d", "e");
+        Stream s3 = lst.stream();
+        Set s = Set.of(1, 2, 3);
+        Stream s4 = s.stream();
     }
 }
